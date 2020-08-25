a1df7472f94ddbaf31267868995fb9f1cc01f04c
Officiallys <56452810+Officiallys@users.noreply.github.com>
2020-08-25 12:15:45 +0200

    Start of workshop css

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 'use strict';
 
 let mainNav = document.getElementById('main-nav');
-mainNav.className += 'hidden';
 
 const hamburgerMenu = document.getElementById('hamburger-menu');
 hamburgerMenu.addEventListener('click', function(){
-    mainNav.classList.toggle('hidden');
+    if (mainNav.style.display === "none") {
+        mainNav.style.display = "block";
+      } else {
+        mainNav.style.display = "none";
+      }
     this.src = '';
 });
