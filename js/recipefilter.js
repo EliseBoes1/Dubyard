d32c680ea1eba0cb43bd3c1e74e38195b3c3b101
Officiallys <56452810+Officiallys@users.noreply.github.com>
2020-08-29 09:25:41 +0200

    Start of recipe filter

---
+++ js/recipefilter.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const allFilters = Array.from(document.getElementById('filter-recipes').getElementsByTagName('li'));
+
+const selectedType = Array.from(document.getElementById('filter-type').getElementsByTagName('li'));
+const selectedEdition = Array.from(document.getElementById('filter-edition').getElementsByTagName('li'));
+const selectedTime = Array.from(document.getElementById('filter-time').getElementsByTagName('li'));
+
+const selectedFilters = {
+    type:'',
+    edition:'',
+    time:''
+}
+
+const addSelected = (selectedType, value) =>{
+    console.log(selectedType, value)
+    selectedFilters.selectedType = value;
+}
+
+const evalSelected = element =>{
+    element.addEventListener('click', function(){
+        const valueToFilter = this.innerHTML.split(' ')[0].toLocaleLowerCase();
+        toggleSelected(element);
+        addSelected(element.parent.id, valueToFilter);
+    });
+}
+
+const toggleSelected = (element) =>{
+    const siblings = Array.from(element.parentElement.children);
+    siblings.forEach(sibling=>{
+        sibling.classList -= 'selected';
+    });
+    element.classList.toggle('selected');
+}
+
+selectedType.forEach(type =>{
+    evalSelected(type);
+});
+
+selectedEdition.forEach(type =>{
+    evalSelected(type);
+});
+
+selectedTime.forEach(type =>{
+    evalSelected(type);
+});
+
